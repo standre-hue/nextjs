2cf241a054ea5e26422adae20732544bcfe014d3
Jr Stan <66187962+standre-hue@users.noreply.github.com>
2022-06-19 18:23:36 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
   return (
     <div className={styles.container}>
+    
       
     </div>
   )
